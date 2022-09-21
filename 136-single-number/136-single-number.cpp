5be780a3113d468d95c839f227d34f2c9f6e078b
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2022-09-21 22:56:10 +0530

    Time: 23 ms (84.19%), Space: 17 MB (53.66%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int res=0;
-        for(int i=0;i<nums.size();i++){res=res^nums[i];
-                                                 cout<<res<<endl;
-                                                }
-            return res;        
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1])
+                i+=1;
+            else
+                return nums[i];
+            
+        }
+        return nums[nums.size()-1];       
     }
 };
