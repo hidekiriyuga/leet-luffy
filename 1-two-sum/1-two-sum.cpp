5b7806a7ea9c0d0720b57eaca2a740f4bfcc5fa4
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2022-09-24 23:05:25 +0530

    Time: 38 ms (45.83%), Space: 12.7 MB (5.37%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        vector<int> ans;
+        int c;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]=i+1;
+        }
+        for(int i=0;i<nums.size();i++){
+            int c=target-nums[i];
+            if(mp[c]!=0 && mp[c]!=i+1){
+                ans.push_back(i);
+                ans.push_back(mp[c]-1);
+                return ans;
+            }
+        }
+        return ans;
+            
+    }
+};
