6a681aaee9c2b5c1efb3426fb95ab233cce864f7
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2022-10-27 12:32:39 +0530

    Time: 9 ms (39.87%), Space: 11.2 MB (28.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l=0,h=nums.size()-1;
-        int mid;
-        while(l<=h){
+        //int l=0,h=nums.size()-1;
+        //int mid;
+        //striver
+       /* while(l<=h){
             mid=(l+h)/2;
             if(nums[mid]==target)return mid;
             if(nums[l]<=nums[mid]){
@@ -14,6 +15,37 @@ public:
                 if(nums[mid]<=target && nums[h]>=target)l=mid+1;
                 else h=mid-1;
             }
+        }
+        return -1;
+    }*/
+        
+        int pi=pivot(nums);
+        //if(pi==-1)return -1;
+        //if(target==nums[pi])return pi;
+        int ans=bsearch(nums,target,0,pi);
+        
+        if(ans==-1)ans=bsearch(nums,target,pi+1,nums.size()-1);
+        return ans;
+    }
+        //kunal
+    int bsearch(vector<int>& nums, int target,int l,int h){
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]>target)h=mid-1;
+            else l=mid+1;
+        }
+        return -1;
+    }
+    int pivot(vector<int> arr){
+        int l=0,h=arr.size()-1,mid;
+        while(l<=h){
+            mid=(l+h)/2;
+            if(mid<h && arr[mid]>arr[mid+1])return mid;
+            if(mid>l&&arr[mid-1]>arr[mid])return mid-1;
+            if(arr[mid]<=arr[l])h=mid-1;
+            else l=mid+1;
+
         }
         return -1;
     }
