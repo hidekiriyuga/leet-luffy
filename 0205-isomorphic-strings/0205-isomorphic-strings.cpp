50dfceb8125fba4b626ac8a124736624adc1ca93
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2022-11-04 11:24:44 +0530

    Time: 11 ms (69.65%), Space: 6.9 MB (73.75%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isIsomorphic(string str1, string str2) {
+        char hash[256]; 
+        int n1 = str1.size();
+        int n2 = str2.size();
+        
+        // Checking Base case 
+        if(n1 != n2){
+            return false;
+        }
+        
+        // checking str1 to str2 mapping
+        for(int i=0;i<n1;i++){
+            hash[str1[i]] = str2[i];
+        }
+        for(int i=0;i<n1;i++){
+            if(str2[i] != hash[str1[i]]){
+                return false;
+            }
+        }
+        
+        // checking str2 to str1 mapping
+        for(int i=0;i<n1;i++){
+            hash[str2[i]] = str1[i];
+        }
+        for(int i=0;i<n1;i++){
+            if(str1[i] != hash[str2[i]]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+};
