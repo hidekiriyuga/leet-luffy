edf3ec18ac66fd36d048553918ba3233f7624803
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2022-11-16 11:20:56 +0530

    Time: 0 ms (100.00%), Space: 6 MB (97.81%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length())return false;
+        s+=s;
+        if(s.find(goal)!=string::npos)
+                return true;
+            else
+                return false;
+
+    }
+};
