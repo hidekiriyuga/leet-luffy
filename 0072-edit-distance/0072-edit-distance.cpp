5e98e06953c1697b6c79bbf58d50c2bdb33b49aa
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2023-02-26 13:17:26 +0530

    Time: 21 ms (38.06%), Space: 9.1 MB (42.85%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
-    int cal(int i,int j,string &s1,string &s2,vector<vector<int>> &dp){
-        if(i<0)return j+1;
-        if(j<0)return i+1;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i]==s2[j])return dp[i][j]=0+cal(i-1,j-1,s1,s2,dp);
-        return dp[i][j]= 1 + min(cal(i-1,j,s1,s2,dp),
-                    min(cal(i,j-1,s1,s2,dp),
-                       cal(i-1,j-1,s1,s2,dp)));
-    };
+    
     int minDistance(string s1, string s2) {
         int n=s1.size(),m=s2.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
