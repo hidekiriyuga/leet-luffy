90e02232a907b2d2f1dacea7febf97af40ecf3fb
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2023-02-26 13:17:13 +0530

    Time: 12 ms (71.88%), Space: 9 MB (63.20%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -9,9 +9,21 @@ public:
                     min(cal(i,j-1,s1,s2,dp),
                        cal(i-1,j-1,s1,s2,dp)));
     };
-    int minDistance(string word1, string word2) {
-        int n=word1.size(),m=word2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return cal(n-1,m-1,word1,word2,dp);
+    int minDistance(string s1, string s2) {
+        int n=s1.size(),m=s2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++)dp[i][0]=i;
+        for(int j=0;j<=m;j++)dp[0][j]=j;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                        if(s1[i-1]==s2[j-1])dp[i][j]=dp[i-1][j-1];
+           else  dp[i][j]= 1 + min(dp[i-1][j],
+                    min(dp[i][j-1],
+                       dp[i-1][j-1]));
+            }
+        }
+        return dp[n][m];
+
+        
     }
 };
