029f486b21097b9c4723e7703759ccfa7ec17af4
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2023-03-08 18:05:28 +0530

    Time: 47 ms (73.54%), Space: 19 MB (45.12%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int H) {
-      // int N=piles.size();
-        int s=1;
-        int e=INT_MIN;
-        for(int i=0;i<piles.size();i++){
-            //s=min(s,piles[i]);
-            e=max(e,piles[i]);
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int s=1,e=INT_MIN,mid;
+        for(auto i:piles){
+            e=max(e,i);
         }
-        //cout<<s<<" "<<e; 
         while(s<e){
-            int mid=s+(e-s)/2;
             int sum=0;
-            for(auto num:piles){
-                sum+=num/mid;
-                if(num%mid!=0)sum++;
+          mid=s+(e-s)/2;
+            for(int i:piles){
                 
-                /*sum+=ceil(float(num/mid))+1;
-                    float x=float(num/mid);
-                    cout<<e<<" "<<ceil(num/mid)<<"\n";*/
-                
+                sum+=i/mid;
+                if(i%mid!=0)sum++;
             }
-            
-            //if(sum==H)return mid;
-            if(sum<=H)e=mid;
+            if(sum<=h)e=mid;
             else s=mid+1;
         }
         return s;
+        
     }
 };
