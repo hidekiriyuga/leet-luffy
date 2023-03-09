1eac5397760989ddf09671d6255486c04589df07
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2023-03-09 09:46:26 +0530

    Time: 8 ms (76.50%), Space: 7.6 MB (68.59%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,50 +8,21 @@
  */
 class Solution {
 public:
-    int lengthcycle(ListNode* head){
-        ListNode* fast=head;
-        ListNode* slow=head;
-        while(fast!=NULL && fast->next!=NULL){
-            fast=fast->next->next;
-            slow=slow->next;
-            if(slow==fast){
-                ListNode* temp=slow;
-                int len=0;
-                do{
-                    temp=temp->next;
-                    len++;
-                }
-                while(temp!=slow);
-                return len;
-            }
-            
-        }
-        return 0;
-    }
     ListNode *detectCycle(ListNode *head) {
-        int length=0;
-        ListNode* fast=head;
-        ListNode* slow=head;
-        while(fast!=NULL && fast->next!=NULL){
-            fast=fast->next->next;
+        ListNode* slow=head,*fast=head;
+        while(fast!=NULL &&fast->next!=NULL){
             slow=slow->next;
+            fast=fast->next->next;
             if(slow==fast){
-                length=lengthcycle(slow);
                 break;
             }
-            
-    }
-        if(length==0)return NULL;
-        ListNode* f=head;
-        ListNode* s=head;
-        while(length>0){
-            s=s->next;
-            length--;
         }
-        while(f!=s){
-            f=f->next;
-            s=s->next;
+        if(fast==NULL ||fast->next==NULL)return NULL;
+        fast=head;
+        while(fast!=slow){
+            fast=fast->next;
+            slow=slow->next;
         }
-        return s;
+        return fast;
     }
 };
