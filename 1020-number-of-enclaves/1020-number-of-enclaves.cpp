d7bf4c8f52ea3b49b117beee24aa2f4f67b8978a
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2023-06-16 18:22:27 +0530

    Time: 86 ms (49.07%), Space: 33.8 MB (23.40%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,40 +1,41 @@
 class Solution {
-public:
-void dfs(int r,int c,vector<vector<int>> &vis,vector<vector<int>> &grid){
+  public:
+    void dfs(int r,int c,vector<vector<int>> &vis,vector<vector<int>> &grid,int dr[],int dc[]){
         vis[r][c]=1;
         int n = grid.size();
         int m = grid[0].size();
-    int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
         for(int i=0;i<4;i++){
             int nr=r+dr[i];
             int nc=c+dc[i];
             if( nr>=0 &&nr<n && nc>=0 &&nc<m &&!vis[nr][nc]&& grid[nr][nc]==1){
                 //vis[nr][nc]=1;
-                dfs(nr,nc,vis,grid);
+                dfs(nr,nc,vis,grid,dr,dc);
             }
         }
     }
-    int numEnclaves(vector<vector<int>>& grid) {
+    int numEnclaves(vector<vector<int>> &grid) {
+        // Code here
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>> vis(n,vector<int>(m,0));
+         int dr[]={-1,0,1,0};
+        int dc[]={0,1,0,-1};
         for(int j=0;j<m;j++){
             if(!vis[0][j] && grid[0][j]==1){
-                dfs(0,j,vis,grid);
+                dfs(0,j,vis,grid,dr,dc);
             }
             if(!vis[n-1][j] && grid[n-1][j]==1){
-                dfs(n-1,j,vis,grid);
+                dfs(n-1,j,vis,grid,dr,dc);
                 
             }
             
         }
         for(int i=0;i<n;i++){
             if(!vis[i][0] && grid[i][0]==1){
-                dfs(i,0,vis,grid);
+                dfs(i,0,vis,grid,dr,dc);
             }
             if(!vis[i][m-1] && grid[i][m-1]==1){
-                dfs(i,m-1,vis,grid);
+                dfs(i,m-1,vis,grid,dr,dc);
                 
             }
             
@@ -48,6 +49,5 @@ void dfs(int r,int c,vector<vector<int>> &vis,vector<vector<int>> &grid){
             }
         }
         return cnt;
-        
     }
 };
