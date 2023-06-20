4703897700a5ce108ba3645a8d41b38f8effc6ed
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2023-06-20 10:28:44 +0530

    Time: 25 ms (75.25%), Space: 13.4 MB (92.39%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool dfs(int node,int c,vector<int>&col,vector<vector<int>> &graph){
+        col[node]=c;
+        for(auto it:graph[node]){
+            if(col[it]==-1){
+                if(!dfs(it,!c,col,graph))return  false;
+            }
+            else if(col[it]==c)return false;
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> col(n,-1);
+        for(int i=0;i<n;i++){
+            if(col[i]==-1){
+                if(!dfs(i,0,col,graph))return  false;
+            }
+        }
+        return true;
+    }
+};
