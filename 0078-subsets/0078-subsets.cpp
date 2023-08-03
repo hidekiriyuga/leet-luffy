eaa59c827171f1e5256c748f67670c113256cb2a
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2023-08-03 23:30:42 +0530

    Time: 0 ms (100.00%), Space: 7 MB (75.05%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void solve(vector<int>& nums,vector<int>& temp,vector<vector<int>> &ans,int i){
+        ans.push_back(temp);
+        for(int j=i;j<nums.size();j++){
+            temp.push_back(nums[j]);
+            solve(nums,temp,ans,j+1);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+       // sort(nums.begin(),nums.end());
+        solve(nums,temp,ans,0);
+        return ans;
+    }
+};
