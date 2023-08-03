47b9dd213cb6d9ee644143a84ce9c09c2ed4d73f
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2023-08-03 23:30:52 +0530

    Time: 3 ms (60.24%), Space: 7.1 MB (75.05%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -11,7 +11,7 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> temp;
-       // sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());
         solve(nums,temp,ans,0);
         return ans;
     }
