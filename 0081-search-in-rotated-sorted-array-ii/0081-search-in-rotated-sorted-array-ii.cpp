b22c5bcc5a0f71a0ab3e25a295d3112cb36c5432
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2023-08-10 22:36:37 +0530

    Time: 7 ms (53.92%), Space: 14 MB (50.05%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool search(vector<int>& A, int Key) {
-        int l=0,e=A.size()-1;
-        int mid;
-        while(l<e && A[l]==A[l+1])
+    bool search(vector<int>& nums, int target) {
+        
+    int l=0,r=nums.size()-1; 
+        while(l<r && nums[l]==nums[l+1])
         l++;
-        while(e>l && A[e]==A[e-1])e--;
-        while(l<=e){
-             mid=(l+e)/2;
-             if(A[mid]==Key)return true;
-             if(A[l]<=A[mid]){
-                 if(A[l]<=Key && Key<=A[mid])e=mid-1;
-                 else l=mid+1;
-             }
-             else{
-                 if(A[e]>=Key && Key>=A[mid])l=mid+1;
-                 else e=mid-1;
-             }
+        while(r>l && nums[r]==nums[r-1])r--;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(target==nums[mid])return 1;
+            if(nums[l]<=nums[mid]){
+                if(nums[l]<=target&&nums[mid]>=target)r=mid-1;
+                else l=mid+1;
+            }
+            else{
+                if(nums[mid]<=target &&nums[r]>=target)l=mid+1;
+                else r=mid-1;
+            }
         }
-        return false;
+        return 0;
     }
 };
