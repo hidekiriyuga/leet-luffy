736c4f6aa3087b9190ac5e04fc3b22c0eff8af5e
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2023-09-04 10:36:07 +0530

    Time: 7 ms (91.08%), Space: 8.1 MB (69.03%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -6,30 +6,16 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-int pos;
 class Solution {
 public:
-    bool hasCycle(struct ListNode *head) {
-    /*int i;
-    
-    for (i = 0; (i < 10000 + 1) && head; ++i, head = head->next);
-    
-    if (i == 10000 + 1) {
-        return true;
-    }
-    
-    return false;*/
-        
-        
-        
-    ListNode* fast=head;
-    ListNode* slow=head;
-        while(fast!=NULL && fast->next!=NULL){
-            fast=fast->next->next;
-            slow=slow->next;
-            if(fast==slow)return true;
+    bool hasCycle(ListNode *head) {
+        ListNode* f=head;
+        ListNode* s=head;
+        while(f && f->next){
+            s=s->next;
+            f=f->next->next;
+            if(s==f)return 1;
         }
-        return false;
-        
-}
+        return 0;
+    }
 };
